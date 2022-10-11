da8cd210489010ad274c81d17c946ad9a6481155
Stanislav Yordanov <stanislav.yordanov@securax.org>
2022-10-11 14:45:21 +0300

    more changes

--- src/Button.js
+++ src/Button.js
@@ -33,9 +33,9 @@ const SIZES = {
 };
 
 const StyledButton = styled.button`
-  border: 10px solid red;
-  font-size: 20px;
-  //border: 0;
+  //border: 10px solid red;
+  //font-size: 20px;
+  border: 0;
   border-radius: 3em;
   cursor: pointer;
   display: inline-block;
@@ -53,9 +53,8 @@ const StyledButton = styled.button`
   opacity: 1;
   margin: 0;
   background: transparent;
-
-  /*  font-size: ${(props) =>
-    props.size === SIZES.SMALL ? typography.size.s1 : typography.size.s2}px;*/
+  font-size: ${(props) =>
+    props.size === SIZES.SMALL ? typography.size.s1 : typography.size.s2}px;
   font-weight: ${typography.weight.extrabold};
   line-height: 1;
 
